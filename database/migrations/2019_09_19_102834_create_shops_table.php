5f8171a3bc0b32771f650e86650eb6c8ae17d214
ryo-7956 <ryoma.hosoya@gmail.com>
2019-09-19 06:39:13 +0000

    shopsマイグレーション実行

--- database/migrations/2019_09_19_102834_create_shops_table.php
+++ database/migrations/2019_09_19_102834_create_shops_table.php
@@ -15,7 +15,14 @@ class CreateShopsTable extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('shop_name');
+            $table->text('shop_address');
+            $table->text('shop_description');
+            $table->unsignedInteger('shop_admin_id');
+            $table->unsignedInteger('shop_prefecture_id');
             $table->timestamps();
+            $table->foreign('shop_admin_id')->references('id')->on('admins');
+            $table->foreign('shop_prefecture_id')->references('id')->on('prefectures');
         });
     }
 
