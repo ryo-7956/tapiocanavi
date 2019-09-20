e03b38dbb10cc4569077c28e2653b461d9443f12
ryo-7956 <ryoma.hosoya@gmail.com>
2019-09-20 00:07:11 +0000

    reviewマイグレーション

--- database/migrations/2019_09_20_081454_create_reviews_table.php
+++ database/migrations/2019_09_20_081454_create_reviews_table.php
@@ -14,8 +14,14 @@ class CreateReviewsTable extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('review_id');
+            $table->text('review_comment');
+            $table->string('review_date');
+            $table->unsignedInteger('review_shop_id');
+            $table->unsignedInteger('user_id');
             $table->timestamps();
+            $table->foreign('review_shop_id')->references('shop_id')->on('shops')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
