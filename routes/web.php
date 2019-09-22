c1ab0d11766bf1d9f243ca4c3a88df5dc02b118a
ryo-7956 <ryoma.hosoya@gmail.com>
2019-09-22 12:07:53 +0000

    表記追加など

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,37 @@
 
 Auth::routes();
 
+//トップページ
 Route::get('/', function () {
     return view('welcome');
 });
 
+//場所から探す
 Route::group(['prefix' => 'prefecture'], function (){
-Route::get('/', 'PrefectureController@index');
+Route::get('/', 'PrefectureController@index')->name('pref.index');
 Route::get('/{id}', 'PrefectureController@show')->name('pref.show');
 Route::get('/{id}/{shop_id}', 'PrefectureController@shop')->name('pref.shop');
 });
 
+//誰でもみれるユーザー画面
+Route::get('users/{id}', 'UsersController@index')->name('users.show');
+
+//ログインユーザー画面
 Route::group(['prefix' => 'users', 'middleware' => 'auth'], function () {
     Route::get('show/{id}', 'UsersController@show')->name('users.show');
     Route::get('edit/{id}', 'UsersController@edit')->name('users.edit');
     Route::post('update/{id}', 'UsersController@update')->name('users.update');
 });
 
-Route::get('users/{id}', 'UsersController@index')->name('users.show');
-
+//管理人ユーザー機能
 Route::group(['middleware' => 'auth:user'], function() {
     Route::get('/home', 'HomeController@index')->name('home');
 });
-
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/',         function () { return redirect('/admin/home'); });
     Route::get('login',     'Admin\LoginController@showLoginForm')->name('admin.login');
     Route::post('login',    'Admin\LoginController@login');
 });
-
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
     Route::post('logout',   'Admin\LoginController@logout')->name('admin.logout');
     Route::get('home',      'Admin\HomeController@index')->name('admin.home');
