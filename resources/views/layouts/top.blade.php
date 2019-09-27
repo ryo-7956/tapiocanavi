c24a3bc5802b9c2d301ac558eb4b5cb9245d4d2a
ryo-7956 <ryoma.hosoya@gmail.com>
2019-09-27 12:57:02 +0000

    レイアウト修正

---
+++ resources/views/layouts/top.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="Colorlib">
+        <meta name="description" content="#">
+        <meta name="keywords" content="タピオカ、口コミ">
+
+        <title>タピオカなび</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="css/simple-line-icons.css">
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <link rel="stylesheet" href="css/set1.css">
+    </head>
+    <body>
+    <div class="nav-menu">
+        <div class="bg transition">
+            <div class="container-fluid fixed">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                            <a class="navbar-brand" href="/">タピオカなび</a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="icon-menu"></span>
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                                <ul class="navbar-nav">
+                                @guest
+                                <li class="nav-item">
+                                    <a href="{{ route('login') }}" class="nav-link">ログイン</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('register') }}" class="nav-link">新規登録</a>
+                                </li>
+                                @else
+                                <li class="nav-item">
+                                <a href="/users/show/{{ Auth::user()->id }}" class="nav-link" >
+                                    マイページ</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();" class="nav-link">
+                                            ログアウト
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                    @endguest
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+    <footer class="main-block dark-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright">    
+                        <p>Copyright &copy; <a>2019 タピオカなび </a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script>
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 100) {
+                $('.fixed').addClass('is-sticky');
+            } else {
+                $('.fixed').removeClass('is-sticky');
+            };
+        });
+    </script>
+</body>
+</html>
+   
