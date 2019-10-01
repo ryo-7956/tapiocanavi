ab5edd3217be76c59fa70e6f463a526399a19aa1
ryo-7956 <ryoma.hosoya@gmail.com>
2019-10-01 06:04:09 +0000

    seach機能修正

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,21 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.layout1')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                @forelse ($shops as $shop)
+<section class="reserve-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>検索結果</h2>
                 </div>
-                <h6>{{ $shop->shop_name}}</h6>
             </div>
-            @empty
-                        <a>No shops yet</a>
-                        @endforelse
         </div>
-    </div>
-</div>
+    </section>
+
+    <section class="light-bg booking-details_wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 responsive-wrap">
+                    <div class="booking-checkbox_wrap">
+                        <div class="booking-checkbox">
+                          <div class="row">
+                          @forelse ($shops as $shop)
+                            <div class="col-md-4">
+                                <label class="custom-checkbox">
+                                  <span class="ti-check-box"></span>
+                                    <span class="custom-control-description">
+                                        <a href="/prefecture/{{ $shop->prefecture_id }}/{{ $shop->shop_id }}">{{ $shop->shop_name}}</a>
+                                    </span>
+                                </label> 
+                            </div>
+                            @empty
+                            <a>No shops yet
+                            @endforelse
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        
 @endsection
