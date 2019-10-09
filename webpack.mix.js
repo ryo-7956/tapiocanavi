d2486accc3fdf40a31994dd0293f2347c54302e0
ryo-7956 <ryoma.hosoya@gmail.com>
2019-10-09 03:43:25 +0000

    herokuデプロイ

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,17 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+   options({
+      postCss: [
+          require('autoprefixer')
+      ]
+  });
+
+if (mix.config.inProduction) {
+  mix.version();
+} else {
+  mix.browserSync({
+      proxy:     'localhost:8000',
+      startPath: '/'
+  });
+}
