e068091320f7994bb71e525ea4aa76aadb2975d8
ryo-7956 <ryoma.hosoya@gmail.com>
2019-10-15 04:04:32 +0000

    余計なコード削除

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -27,18 +27,3 @@ class SearchController extends Controller
         ]);
     }
 }
-    
-        //$place = $request->input('place');
-
-        //$query = shop::query();
-        
-        //if (!empty($place)) {
-          //  $query->where('shop_address', 'like', '%' . $place . '%')
-            //->orWhere('shop_name', 'Like', '%' . $place . '%');
-        //} else {
-            //return view('welcome');
-        //}
-
-        //$shops = $query->get();
-        //return view('search')
-        //->with('shops', $shops);
