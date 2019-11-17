10f8fec9443b341115aaaf4b7d7ceca5006188cd
ryo-7956 <ryoma.hosoya@gmail.com>
2019-11-17 09:47:02 +0000

    コード整形

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,21 +2,15 @@
 
 @section('content')
 <div class='usershowPage'>
-  <div class='container'>
-    <div class='userInfo'>
-      <div class='userInfo_name'></div>
-    </div>
-
-      <div class='userAction'>
-        <div class="userAction_edit userAction_common">
-          
-      
-          
-          <span><a href="/admin/shop">情報を編集</a></span>
-          
+    <div class='container'>
+        <div class='userInfo'>
+            <div class='userInfo_name'></div>
         </div>
-    
-  </div>
-</div>
-
+        <div class='userAction'>
+            <div class="userAction_edit userAction_common">
+                <span><a href="/admin/shop">情報を編集</a></span>
+            </div>
+        </div>
+    </div>
+<div>
 @endsection
