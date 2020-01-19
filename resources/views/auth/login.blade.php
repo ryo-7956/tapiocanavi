0b96c321ab2f1efd432b7c1974fcd3414e3fed51
ryo-7956 <ryoma.hosoya@gmail.com>
2020-01-19 23:34:41 +0000

    エラーメッセージ表記、日本語化

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,11 +12,21 @@
             {{ csrf_field() }}
             <div class="form-group">
                 <label>メールアドレス</label>
-                <input type="email" name="email" class="form-control" placeholder="メールアドレスを入力してください" autofocus>
+                <input type="email" name="email" class="form-control" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
             </div>
             <div class="form-group">
                 <label>パスワード</label>
-                <input type="password" name="password" class="form-control" placeholder="パスワードを入力してください">
+                <input type="password" name="password" class="form-control" value="{{ old('password') }}" required autofocus>
+                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
             </div>
             <div class="form-group text-center">
                 <button type="submit" class="loginBtn">ログイン</button>
